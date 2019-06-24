08229ffbf857c797b6694867eedea8ea88270684
thanhnm <thanhbka@yahoo.com>
2019-06-24 18:22:47 +0700

    update time, modal cms

--- project/CMS_PS4_2019/src/components/views/Customer.vue
+++ project/CMS_PS4_2019/src/components/views/Customer.vue
@@ -3,56 +3,65 @@
     <div class="row">
       <div class="col-xs-12">
         <h2>List customers</h2>
-        <ps4-detail v-if="showModal">
+        <ps4-detail v-if="showModal" :transaction="showModal">
             <h3 slot="header" class="modal-title">
-                Modal title
+              Chi tiết {{getPs4.id}}
             </h3>
+
+             <div slot="body">
+               <p>Máy số: {{getPs4.id}}</p>
+               <p>{{getPs4.origin}}</p>
+               <p>Bắt đầu: {{getPs4.start}}</p>
+               <p>Danh sách dịch vụ:</p>
+             </div>
                 
             <div slot="footer">
                 <button type="button" class="btn btn-outline-info" @click="closeModal()"> Close </button>
-                <button type="button" class="btn btn-primary" data-dismiss="modal" @click="submitAndClose()"> Submit </button>
+                <button type="button" class="btn btn-primary" data-dismiss="modal" @click="submitAndClose()"> Update </button>
+                <button type="button" class="btn btn-primary" data-dismiss="modal" @click="submitAndClose(getPs4.id)"> Checkout </button>
             </div>
         </ps4-detail>
         <button type="button" class="btn btn-primary" @click="openModal('thanhnm')">Open Modal</button>
+        <button type="button" class="btn btn-primary" @click="forceRerender()">Re-render Component</button>
       </div>
 
       <!-- PS4 boxes -->
       <div class="col-md-3 col-sm-6 col-xs-12">
         <ps4-box
-          color-class="bg-green"
           :icon-classes="['ion', 'ion-ios-game-controller-a-outline']"
           text="Máy 1"
-          number="960"
+          number="ps4_01"
           :openModal="openModal"
           @created="handleCreate"
+          :key="componentKey"
         ></ps4-box>
       </div>
       <div class="col-md-3 col-sm-6 col-xs-12">
         <ps4-box
-          color-class="bg-green"
           :icon-classes="['ion', 'ion-ios-football-outline']"
           text="Máy 2"
-          number="760"
+          number="ps4_02"
+          :ps4-start="true"
           :openModal="openModal"
           @created="handleCreate"
+          :key="componentKey"
         ></ps4-box>
       </div>
       <div class="col-md-3 col-sm-6 col-xs-12">
         <ps4-box
-          color-class="bg-green"
           :icon-classes="['ion', 'ion-ios-game-controller-b-outline']"
           text="Máy 3"
-          number="760"
+          number="ps4_03"
+          :ps4-start="true"
           :openModal="openModal"
           @created="handleCreate"
         ></ps4-box>
       </div>
       <div class="col-md-3 col-sm-6 col-xs-12">
         <ps4-box
-          color-class="bg-black"
           :icon-classes="['ion', 'ion-ios-game-controller-b-outline']"
           text="Máy 4"
-          number="820"
+          number="ps4_04"
           :openModal="openModal"
           @created="handleCreate"
         ></ps4-box>
@@ -70,24 +79,33 @@ export default {
   name: 'Customer',
   data() {
     return {
-      showModal: false
+      showModal: false,
+      listPS4: [],
+      componentKey: 0,
+      getPs4: {}
     }
   },
   methods: {
     openModal(val = 'Open') {
       console.log('Call OPEN Parent')
-      console.log(val)
+      this.getPs4 = typeof val === 'object' ? val : {}
       this.showModal = true
     },
     closeModal() {
       console.log('Call CLOSE Parent')
       this.showModal = false
     },
-    submitAndClose() {
-      alert('123')
+    submitAndClose(pNumber = 1) {
+      let result = confirm('Bạn có muốn thực hiện thanh toán ?')
+      if (result === true) {
+        alert(pNumber)
+      }
     },
-    handleCreate(event, value) {
+    handleCreate(value) {
       console.log('Child has been created.', value)
+    },
+    forceRerender() {
+      this.componentKey += 1
     }
   },
   components: {
--- project/CMS_PS4_2019/src/components/widgets/Ps4Box.vue
+++ project/CMS_PS4_2019/src/components/widgets/Ps4Box.vue
@@ -5,12 +5,19 @@
     </span>
     <div class="info-box-content">
       <span class="info-box-text">{{text}}</span>
-      <span class="info-box-number">{{number}}</span>
+      <span>Trạng thái: </span>
+      <span :class="localPs4Start ? 'z-ps4-on' : ''">
+        {{localPs4Start ? 'Bật' : 'Tắt'}}
+      </span>
 
       <span class="info-box-more z-more">
-        <a href="#" class="btn btn-xs" @click="open_modal()">
+        <a v-if="localPs4Start" href="#" class="btn btn-xs" @click="open_modal()">
           More info
           <i class="fa fa-arrow-circle-right"></i>
+        </a>
+        <a v-else href="#" class="btn btn-xs" @click="play(number)">
+          START
+          <i class="fa fa-play-circle-o"></i>
         </a>
       </span>
     </div>
@@ -18,11 +25,22 @@
 </template>
 
 <script>
+import moment from 'moment'
+
 export default {
   name: 'Ps4Box',
   data() {
     return {
-      msg: 'I am the child.'
+      msg: 'I am the child.',
+      colorClass: 'bg-black',
+      ps: {
+        id: 1, // so may
+        origin: '', // may dang choi truoc khi chuyen
+        start: Date.now(),
+        end: '',
+        items: {}
+      },
+      localPs4Start: this.ps4Start
     }
   },
   props: {
@@ -38,27 +56,67 @@ export default {
       type: Array,
       default: []
     },
-    colorClass: {
-      type: String,
-      default: 'bg-default'
-    },
-    openModal: Function
+    openModal: { type: Function }, // openModal: Function,
+    ps4Start: {
+      type: Boolean,
+      default: false
+    }
+  },
+  computed: {
+
   },
   created() {
     // goi ham cha
     this.$emit('created', this.msg) // communicate to parent components
+    console.log('PS Created')
+  },
+  mounted() {
+    console.log('PS Mounted', this.msg)
+    this.toggleOnClass()
   },
   methods: {
     open_modal() {
       console.log('Call OPEN Child')
-      this.openModal(this.msg) // ham tu cha truyen vao con
+      let local = window.localStorage.getItem(this.number) || 'null'
+      let localPS = JSON.parse(local)
+      if (localPS) {
+        this.openModal(localPS) // ham tu cha truyen vao con
+      }
+    },
+    play(pNumber = 1) {
+      let start = moment().subtract(2, 'hours')
+      this.ps = {
+        id: pNumber,
+        origin: pNumber,
+        start: start,
+        end: moment().format(moment.HTML5_FMT.TIME),
+        elapsed: moment().diff(start, 'minutes'),
+        current: moment().subtract(5, 'minutes')
+      }
+      window.localStorage.setItem(pNumber, JSON.stringify(this.ps))
+      this.toggleOnPS4()
+      this.toggleOnClass()
+    },
+    toggleOnPS4() {
+      this.localPs4Start = !this.localPs4Start
+    },
+    toggleOnClass() {
+      this.colorClass = this.localPs4Start ? 'bg-green' : 'bg-black'
     }
   }
 }
 </script>
 
-<style>
+<style scoped>
+.info-box-text {
+  font-weight: bold;
+}
 .z-more {
   margin-top: 10px;
+}
+
+.z-ps4-on {
+  font-weight: bold;
+  color: #42b527;
 }
 </style>
