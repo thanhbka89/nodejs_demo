a57786c2151bead7a126ee3727f3ed04b154fcdc
Nguyễn Mai Thành (VT-CTCNPMVST-TTDVTKPMSAP) <thanhnm@vingroup.net>
2019-07-25 17:23:36 +0700

    url change then view update

--- project/CMS_PS4_2019/src/components/App.vue
+++ project/CMS_PS4_2019/src/components/App.vue
@@ -1,7 +1,7 @@
 <template>
   <transition name="fade">
     <div id="app">  
-      <router-view></router-view>  
+      <router-view :key="$route.fullPath"></router-view>  
     </div>
   </transition>
 </template>
