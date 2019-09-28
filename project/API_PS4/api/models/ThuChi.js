c63db45b7316c9a61f0c1a5de64e43585441d9ff
thanhnm <thanhbka@yahoo.com>
2019-09-28 16:45:53 +0700

    [api] Thu Chi

---
+++ project/API_PS4/api/models/ThuChi.js
@@ -0,0 +1,109 @@
+import DB from './Database'
+const TABLE_NAME = 'thu_chi'
+const OBJ_DB = new DB
+const STATUS_ACTIVE = 1
+const STATUS_INACTIVE = 0
+
+class ThuChi {
+    constructor(obj) {
+        this.code = obj.code
+        this.name = obj.name
+        this.gia_nhap = obj.gia_nhap
+        this.quantity = obj.quantity
+        this.type = obj.type
+        this.description = obj.description
+        this.status = obj.status
+        this.created_by = obj.created_by || 'SYSTEM'
+        this.updated_by = obj.updated_by || 'SYSTEM'
+        this.updated_at = new Date
+    }
+
+    static getCondition(input) {
+        let $where = 'WHERE'
+        let check = 0 // điều kiện đầu tiên (check == 0)
+        // thì ko cần dùng phép AND. Còn là điều kiện thứ N thì phải có AND
+        const {code, type, status, from, to} = input || {}
+        if (code) {
+			$where = $where.concat(` ${check ? 'AND' : ''} code = '${code}'`)
+			check ++
+        }
+        if (type) {
+			$where = $where.concat(` ${check ? 'AND' : ''} type = ${type}`)
+			check ++
+        }
+        if (status) {
+			$where = $where.concat(` ${check ? 'AND' : ''} status = ${status}`)
+			check ++
+        }
+        if (from) {
+            $where = $where.concat(` ${check ? 'AND' : ''} created_at >= '${from}'`)
+            check ++
+        }
+        if (to) {
+            $where = $where.concat(` ${check ? 'AND' : ''} created_at <= '${to}'`)
+            check ++
+        }
+        
+        return {
+            query: $where,
+            hasWhere: check ? true : false // kiem tra xem co dieu kien ko
+        }
+    }
+
+    static async getAll() {
+        let sql = `SELECT * FROM ${TABLE_NAME}`
+
+        return OBJ_DB.query(sql)
+    }
+
+    static async paginate({page = 1, limit = 5, code, type, status, from, to}) {
+        let start = 0
+        page = parseInt(page, 10) || 1
+		limit  = parseInt(limit, 10)  || 5
+        if (page > 1) {
+            start = (page - 1) * limit
+        }
+        const condition = this.getCondition({code, type, status, from, to})
+        let sql = `SELECT * FROM ${TABLE_NAME} ${condition.hasWhere ? condition.query : ''} ORDER BY id DESC LIMIT ? OFFSET ?`
+        return OBJ_DB.query(sql, [limit, start])
+    }
+
+    static async getById(id) {
+        let sql = `SELECT * FROM ${TABLE_NAME} WHERE id = ?`
+
+        return OBJ_DB.query(sql, [id])
+    }
+
+    static async create(newObj) {
+        let sql = `INSERT INTO ${TABLE_NAME} SET ?`
+
+        return OBJ_DB.query(sql, newObj)
+    }    
+
+    static async update(id, obj) {
+        let sql = `UPDATE ${TABLE_NAME} SET ? WHERE id = ?`
+
+        return OBJ_DB.query(sql, [obj, id])
+    }
+
+    static async remove(id) {
+        let sql = `DELETE FROM ${TABLE_NAME} WHERE id = ?`
+
+        return OBJ_DB.query(sql, [id])
+    }
+
+    static async deleteSoft(id) {
+        let sql = `UPDATE ${TABLE_NAME} SET status = ? WHERE id = ?`
+
+        return OBJ_DB.query(sql, [STATUS_INACTIVE, id])
+    }
+
+    static async count(input) {
+        const condition = this.getCondition(input)
+        let sql = `SELECT COUNT(*) AS count FROM ${TABLE_NAME} ${condition.hasWhere ? condition.query : ''}`
+        
+        return OBJ_DB.query(sql)
+    }
+}
+
+export default ThuChi
---
+++ project/API_PS4/api/routes/thuchi.js
@@ -0,0 +1,112 @@
+import { Router } from 'express'
+import ThuChi from '../models/ThuChi'
+const router = Router()
+
+router.get('/', async (req, res) => {
+    try {
+      const result = await ThuChi.getAll()
+      return res.json({
+        success: true,
+        data: result
+      })
+    } catch (e) {
+      return res.json({
+        success: false,
+        data: e
+      })
+    }
+})
+
+router.post('/', async (req, res) => {
+    try {
+      req.body.created_by = req.decoded.username || null
+      let result = await ThuChi.create(new ThuChi(req.body))
+      
+      return res.json({
+        success: true,
+        data: 'Insert success'
+      })
+    } catch (e) {
+      return res.json({
+        success: false,
+        data: e
+      })
+    }
+})
+
+router.route('/:id')
+.get( async (req, res) => {
+    try {
+      const result = await ThuChi.getById(req.params.id)
+      return res.json({
+        success: true,
+        data: result
+      })
+    } catch (e) {
+      return res.json({
+        success: false,
+        data: e
+      })
+    }
+})
+.put(async (req, res) => {
+    try {
+      const result = await ThuChi.update(req.params.id, new ThuChi(req.body))
+      return res.json({
+        success: true,
+        data: result
+      })
+    } catch (e) {
+      return res.json({
+        success: false,
+        data: e
+      })
+    }
+})
+.delete(async (req, res) => {
+    try {
+      const result = await ThuChi.deleteSoft(req.params.id)
+      return res.json({
+        success: true,
+        data: result
+      })
+    } catch (e) {
+      return res.json({
+        success: false,
+        data: e
+      })
+    }
+})
+
+router.get('/p/:page', async (req, res) => {
+  try {
+    req.query.page = req.params.page
+    const result = await ThuChi.paginate(req.query)
+    return res.json({
+      success: true,
+      data: result
+    })
+  } catch (e) {
+    return res.json({
+      success: false,
+      data: e
+    })
+  }
+})
+
+router.get('/get/count', async (req, res) => {
+  try {
+    const result = await ThuChi.count(req.query)
+    return res.json({
+			success: true,
+			data: result[0].count
+		})
+  } catch (e) {
+    return res.json({
+      success: false,
+      data: e
+    })
+  }
+})
+
+export default router
